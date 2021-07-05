557abdb7a29fa6e3ad5ef5c4a42b5dd9bb9d7398
zubair-pc\zubair <szubair@gmail.com>
2021-07-05 17:58:08 +0500

    Route is configured in gateway project

---
+++ OrganicShop_Backend/src/main/kotlin/com/example/OrganicShop_Backend/RoutingConfigrations.kt
@@ -0,0 +1,48 @@
+package com.example.OrganicShop_Backend
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RoutingConfigrations {
+
+    @Value("\${users.Path}")
+    var users: String?=null;
+
+    @Value("\${orders.Path}")
+    var orders: String?=null;
+
+    @Value("\${products.Path}")
+    var products:String? =null;
+
+    @Value("\${categories.Path}")
+    var categories:String? =null;
+
+
+    @Bean
+    fun myRoutes(builder: RouteLocatorBuilder): RouteLocator? {
+        return builder.routes()
+            .route { r ->
+                r.path("/users/**")
+                    .uri(users)
+            }
+
+            .route { r ->
+                r.path("/orders/**")
+                    .uri(orders)
+            }
+            .route { r ->
+                r.path("/products/**")
+                    .uri(products)
+            }
+            .route { r ->
+                r.path("/categories/**")
+                    .uri(categories)
+            }
+            .build()
+    }
+
+}
