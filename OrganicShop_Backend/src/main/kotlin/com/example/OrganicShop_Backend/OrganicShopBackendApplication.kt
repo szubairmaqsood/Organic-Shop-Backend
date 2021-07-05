c1647775361438376183fb38b12f639f57d10a0c
zubair-pc\zubair <szubair@gmail.com>
2021-07-05 17:16:38 +0500

    Project for cloud gateway is created

---
+++ OrganicShop_Backend/src/main/kotlin/com/example/OrganicShop_Backend/OrganicShopBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.OrganicShop_Backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrganicShopBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrganicShopBackendApplication>(*args)
+}
---
+++ OrganicShop_Backend/src/test/kotlin/com/example/OrganicShop_Backend/OrganicShopBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.OrganicShop_Backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrganicShopBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
